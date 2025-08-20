f158abbe28b8b0da2a6466af206751f87139fb83
Akuearias <akuearias@gmail.com>
2025-08-20 19:06:39 -0400

    Aug 19-20

---
+++ weather4.sql
@@ -0,0 +1,4 @@
+select (
+(select count(CITY) from STATION) -
+(select count(distinct CITY) from STATION)
+) as S
---
+++ weather5.sql
@@ -0,0 +1,2 @@
+select CITY, length(CITY) from STATION where length(CITY) = (select min(length(CITY)) from STATION ) order by CITY ASC limit 1;
+select CITY, length(CITY) from STATION where length(CITY) = (select max(length(CITY)) from STATION ) order by CITY ASC limit 1;
